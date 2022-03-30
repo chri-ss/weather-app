1afe07c6611f87b0c8414ee7a0b6ea38fef9555f
Chris <christorper@gmail.com>
2022-03-29 23:18:59 -0700

    Abstract out filtering logic from reportWeather

--- src/processData.js
+++ src/processData.js
@@ -1,26 +1,29 @@
 import getWeather from "./callWeatherAPI";
 
+const filterWeather = (weatherData) => {
+  weatherData = Object.entries(weatherData);
+  weatherData = weatherData.filter(
+    ([k, v]) =>
+      k === "weather" ||
+      k === "main" ||
+      k === "visibility" ||
+      k === "clouds" ||
+      k === "name"
+  );
+  weatherData = Object.fromEntries(weatherData);
+  // console.log(weatherData);
+  // return weatherData;
+  const body = document.querySelector("body");
+  body.textContent = weatherData.name;
+};
+
 const reportWeather = () => {
   const form = document.querySelector("form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const search = form.querySelector("input");
     const newWeather = getWeather(search.value);
-    newWeather.then(
-      (data) => {
-        data = Object.entries(data);
-        data = data.filter(
-          ([k, v]) =>
-            k === "weather" ||
-            k === "main" ||
-            k === "visibility" ||
-            k === "clouds" ||
-            k === "name"
-        );
-        data = Object.fromEntries(data);
-        console.log(data);
-      }
-    );
+    newWeather.then((data) => filterWeather(data));
   });
 };
 
