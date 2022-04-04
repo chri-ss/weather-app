52c1d03ca1a1eab9da5ae530ad97658e5eab2e13
Chris <christorper@gmail.com>
2022-04-03 23:54:02 -0700

    Add labels to weather fields

--- src/DOM.js
+++ src/DOM.js
@@ -54,9 +54,12 @@ const makeWeatherContainer = () => {
 const makeTemp = () => {
   temp.textContent = "-";
   const wrapper = document.createElement("div");
+  const tempLabel = document.createElement("div");
+  tempLabel.textContent = "Temperature";
   const tempImg = document.createElement("img");
   tempImg.src = tempIcon;
   temp.classList.add("temp");
+  wrapper.appendChild(tempLabel);
   wrapper.appendChild(tempImg);
   wrapper.appendChild(temp);
   weatherContainer.appendChild(wrapper);
@@ -65,9 +68,12 @@ const makeTemp = () => {
 const makeHighLow = () => {
   highLow.textContent = "-";
   const wrapper = document.createElement("div");
+  const highLowLabel = document.createElement("div");
+  highLowLabel.textContent = "High & Low";
   const highLowImg = document.createElement("img");
   highLowImg.src = highLowIcon;
   highLow.classList.add("high-low");
+  wrapper.appendChild(highLowLabel);
   wrapper.appendChild(highLowImg);
   wrapper.appendChild(highLow);
   weatherContainer.appendChild(wrapper);
@@ -76,9 +82,12 @@ const makeHighLow = () => {
 const makeWeather = () => {
   weather.textContent = "-";
   const wrapper = document.createElement("div");
+  const weatherLabel = document.createElement("div");
+  weatherLabel.textContent = "Weather";
   const weatherImg = document.createElement("img");
   weather.classList.add("weather");
   weatherImg.src = weatherIcon;
+  wrapper.appendChild(weatherLabel);
   wrapper.appendChild(weatherImg);
   wrapper.appendChild(weather);
   weatherContainer.appendChild(wrapper);
@@ -87,9 +96,12 @@ const makeWeather = () => {
 const makeHumidity = () => {
   humidity.textContent = "-";
   const wrapper = document.createElement("div");
+  const humidityLabel = document.createElement("div");
+  humidityLabel.textContent = "Humidity";
   const humidityImg = document.createElement("img");
   humidity.classList.add("humidity");
   humidityImg.src = humidityIcon;
+  wrapper.appendChild(humidityLabel);
   wrapper.appendChild(humidityImg);
   wrapper.appendChild(humidity);
   weatherContainer.appendChild(wrapper);
@@ -98,20 +110,26 @@ const makeHumidity = () => {
 const makeVisibility = () => {
   visibility.textContent = "-";
   const wrapper = document.createElement("div");
+  const visibilityLabel = document.createElement("div");
+  visibilityLabel.textContent = "Visibility";
   const visibilityImg = document.createElement("img");
   visibility.classList.add("visibility");
   visibilityImg.src = visibilityIcon;
+  wrapper.appendChild(visibilityLabel);
   wrapper.appendChild(visibilityImg);
   wrapper.appendChild(visibility);
   weatherContainer.appendChild(wrapper);
 };
 
 const makeClouds = () => {
-  clouds.textContent = '-';
+  clouds.textContent = "-";
   const wrapper = document.createElement("div");
+  const cloudLabel = document.createElement("div");
+  cloudLabel.textContent = "Cloud Percentage";
   const cloudsImg = document.createElement("img");
   clouds.classList.add("clouds");
   cloudsImg.src = cloudIcon;
+  wrapper.appendChild(cloudLabel);
   wrapper.appendChild(cloudsImg);
   wrapper.appendChild(clouds);
   weatherContainer.appendChild(wrapper);
@@ -140,7 +158,7 @@ const makeDOM = () => {
 
 const fillDOM = (weatherData) => {
   city.textContent = weatherData.name;
-  temp.textContent = weatherData.main.temp;
+  temp.textContent = `${weatherData.main.temp}°C`;
   highLow.textContent = `HI: ${weatherData.main.temp_max}°C LOW: ${weatherData.main.temp_min}°C`;
   weather.textContent = weatherData.weather[0].description;
   humidity.textContent = weatherData.main.humidity;
